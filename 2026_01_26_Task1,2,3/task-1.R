b908c37274deb3ee8143ca2b2a34ee5625044aaa
Aliff Haiqal <149134474+ArKizame@users.noreply.github.com>
2026-01-26 11:45:23 +0800

    Add files via upload

---
+++ 2026_01_26_Task1,2,3/task-1.R
@@ -0,0 +1,9 @@
+x<-1
+count<-0
+while (count<20) {
+  x<-x^2
+  x=x+1
+  count<-count+1
+  print(x)
+}
+
---
+++ 2026_01_26_Task1,2,3/task-3.R
@@ -0,0 +1,4 @@
+r=readline(prompt="Enter radius of circle: ")
+radius <- as.numeric(r)
+circle=pi*radius**2
+print(paste("The radius of the circle is,",circle))
